8c359e83b9dae29753961b723e75a0e2fed91884
Austin Hrin <austinhrin@gmail.com>
2023-11-29 00:56:17 -0500

    added second strip to fade between

--- main.py
+++ main.py
@@ -10,29 +10,33 @@ from time import sleep
 #blue = PWM(Pin(1))
 #red = PWM(Pin(2))
 strip1_green = PWM(Pin(3))
-#strip2_green = PWM(Pin(3))
+strip2_green = PWM(Pin(15))
 #warm_white = PWM(Pin(4))
 
 #cool_white.freq(500)
 #blue.freq(10000)
 #red.freq(500)
 strip1_green.freq(1000)
-#strip2_green.freq(1000)
+strip2_green.freq(1000)
 #warm_white.freq(500)
 
 # make it fade
 fade_sleep = 0.0001 #0.0001
-count_steps = 2
+count_steps = 5
 duty_range = 65025
 
 while True:
     for duty in range(0, duty_range, count_steps):
+        # fade in
         strip1_green.duty_u16(duty)
-        #reverse_duty = duty_range - duty
-        #strip2_green.duty_u16(duty)
+        # fade out
+        reverse_duty = duty_range - duty
+        strip2_green.duty_u16(reverse_duty)
         sleep(fade_sleep)
     for duty in range(duty_range, 0, -count_steps):
+        # fade out
         strip1_green.duty_u16(duty)
-        #reverse_duty = duty_range + duty
-        #strip2_green.duty_u16(duty)
+        # fade in
+        reverse_duty = duty_range - duty
+        strip2_green.duty_u16(reverse_duty)
         sleep(fade_sleep)
