e48099c64e402d6add24c6ebbf8422c1e8b37503
Austin Hrin <austinhrin@gmail.com>
2023-12-01 20:42:01 -0500

    added blue fade option

--- main.py
+++ main.py
@@ -5,6 +5,7 @@ from time import sleep
 # alternating_green_fade will alternate between both strips fading green
 # alternating_green_red_fade will alternate green/red on each strip back and forth fading
 # flash_red_green_fade will keep both the strips the same color (red/green) instead of alternating and will fade between them
+# blue_fade just a blue fade in/out to test if blue works
 color_mode = "flash_red_green_fade"
 
 # turn on the onboard led
@@ -14,29 +15,29 @@ color_mode = "flash_red_green_fade"
 
 strip1_green = PWM(Pin(2))
 strip1_red = PWM(Pin(3))
-#strip1_blue = PWM(Pin(3))
+strip1_blue = PWM(Pin(4))
 
 strip2_green = PWM(Pin(10))
 strip2_red = PWM(Pin(11))
-#strip2_blue = PWM(Pin(12))
+strip2_blue = PWM(Pin(12))
 
 freq = 1000
 start_freq = 100
 strip1_green.freq(start_freq)
 strip1_red.freq(start_freq)
-#strip1_blue.freq(start_freq)
+strip1_blue.freq(start_freq)
 
 strip2_green.freq(start_freq)
 strip2_red.freq(start_freq)
-#strip2_blue.freq(start_freq)
+strip2_blue.freq(start_freq)
 
 start_duty = 0
 strip1_green.duty_u16(start_duty)
 strip1_red.duty_u16(start_duty)
-#strip1_blue.duty_u16(start_duty)
+strip1_blue.duty_u16(start_duty)
 strip2_green.duty_u16(start_duty)
 strip2_red.duty_u16(start_duty)
-#strip2_blue.duty_u16(start_duty)
+strip2_blue.duty_u16(start_duty)
 
 # make it fade
 fade_sleep = 0.0001 #0.0001
@@ -104,6 +105,10 @@ while True:
             sleep(fade_sleep)
         strip1_red.freq(100)
     if color_mode == "flash_red_green_fade":
+        strip1_green.freq(freq)
+        strip1_red.freq(freq)
+        strip2_green.freq(freq)
+        strip2_red.freq(freq)
         # red
         for duty in range(duty_min, duty_range, count_steps):
             # fade in
@@ -126,3 +131,16 @@ while True:
             strip2_green.duty_u16(duty)
         strip1_green.duty_u16(0)
         strip2_green.duty_u16(0)
+    if color_mode == "blue_fade":
+        strip1_red.duty_u16(0)
+        strip2_red.duty_u16(0)
+        strip1_green.duty_u16(0)
+        strip2_green.duty_u16(0)
+        for duty in range(duty_min, duty_range, count_steps):
+            # fade in
+            strip1_blue.duty_u16(duty)
+            strip2_blue.duty_u16(duty)
+        for duty in range(duty_range, duty_min, -count_steps):
+            # fade out
+            strip1_blue.duty_u16(duty)
+            strip2_blue.duty_u16(duty)
